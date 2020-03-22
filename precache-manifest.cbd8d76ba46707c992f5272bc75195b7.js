dbab1636a7502837e442c245bc0fd510796a734f
ruebel <randy.uebel@gmail.com>
2020-03-22 09:28:35 -0400

    Updates

---
+++ precache-manifest.cbd8d76ba46707c992f5272bc75195b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6286c0e08cf32902d9c46c84e980732e",
+    "url": "/dropbox-demo-tape-web/index.html"
+  },
+  {
+    "revision": "17008c2c90b9e0771ee0",
+    "url": "/dropbox-demo-tape-web/static/css/main.ea119a09.chunk.css"
+  },
+  {
+    "revision": "d080b1d81749fc773385",
+    "url": "/dropbox-demo-tape-web/static/js/2.768750a3.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/dropbox-demo-tape-web/static/js/2.768750a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17008c2c90b9e0771ee0",
+    "url": "/dropbox-demo-tape-web/static/js/main.926e2321.chunk.js"
+  },
+  {
+    "revision": "0c466b4118587434adb8",
+    "url": "/dropbox-demo-tape-web/static/js/runtime-main.ece9a5b1.js"
+  }
+]);
