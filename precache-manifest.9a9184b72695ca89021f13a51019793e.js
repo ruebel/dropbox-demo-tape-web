284b8bb2dea38e13cbb3535a8963ca198683be80
Randy Uebel <randy.uebel@zapier.com>
2020-05-26 11:14:34 -0400

    Updates

--- precache-manifest.9a9184b72695ca89021f13a51019793e.js
+++ precache-manifest.9a9184b72695ca89021f13a51019793e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "49f1d8e1ae770f3c500ecf7b4b0a888b",
+    "revision": "c2bdc581325a9bdd631fa508bd750c9b",
     "url": "/dropbox-demo-tape-web/index.html"
   },
   {
-    "revision": "d80d046b1b53e3b2714f",
+    "revision": "0f162a10c97eb3a61c96",
     "url": "/dropbox-demo-tape-web/static/css/main.a18d52f2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dropbox-demo-tape-web/static/js/2.b3f8da7b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d80d046b1b53e3b2714f",
-    "url": "/dropbox-demo-tape-web/static/js/main.ec6343ff.chunk.js"
+    "revision": "0f162a10c97eb3a61c96",
+    "url": "/dropbox-demo-tape-web/static/js/main.7df93cf8.chunk.js"
   },
   {
     "revision": "0c466b4118587434adb8",
