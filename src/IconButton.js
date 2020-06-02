a473a4089efbf0285863480aff520724139e7ab6
Randy Uebel <randy.uebel@zapier.com>
2020-06-02 07:52:32 -0400

    :lipstick: fix button centering

--- src/IconButton.js
+++ src/IconButton.js
@@ -3,25 +3,26 @@ import styled from "styled-components";
 const size = 30;
 
 export default styled.button`
-  background-color: ${p => p.theme.color.background};
-  border: ${p => (p.hideBorder ? "none" : "1px solid white")};
+  align-items: center;
+  background-color: ${(p) => p.theme.color.background};
+  border: ${(p) => (p.hideBorder ? "none" : "1px solid white")};
   border-radius: 50%;
   color: white;
   cursor: pointer;
   display: flex;
-  height: ${p => p.size || size}px;
+  height: ${(p) => p.size || size}px;
   justify-content: center;
   outline: none;
-  transition: ${p => p.theme.transition};
-  width: ${p => p.size || size}px;
+  transition: ${(p) => p.theme.transition};
+  width: ${(p) => p.size || size}px;
 
   :hover,
   :focus {
     background-color: white;
-    color: ${p => p.theme.color.background};
+    color: ${(p) => p.theme.color.background};
   }
 
-  ${p =>
+  ${(p) =>
     p.disabled
       ? `
     color: ${p.theme.color.disabled};
